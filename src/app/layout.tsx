4a3ef919bc279dc808f72d3ecf1426d8de995ca8
formicilab <formicicreativelab@gmail.com>
2023-09-16 21:01:29 +0700

    fix color background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+// import { Nunito } from 'next/font/google'
 
-const nunito = Nunito({ subsets: ['latin'] })
+// const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import styles from './page.module.css'
 export default function Home() {
   return (
    <div className={styles.container}>
+        <TextFalling />
       <video autoPlay loop muted style={{ width: '100%', height: '100vh' }}>
         <source src='/formici-welcoming-web.mp4' type='video/mp4' />
         </video>
-        <TextFalling />
    </div>
   )
 }
